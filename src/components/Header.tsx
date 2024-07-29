03401d6d9bf9e8d1821a95fc03401d1e02b7c1c6
jaycethanks <jaycethanks@outlook.com>
2024-07-29 12:33:13 +0800

    feat: add github address

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
 import { cn } from "@/lib/utils";
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
 
-export default function Header(){
-    return <>
-    <h1 className={cn("text-2xl","font-semibold","text-foreground")}>FFmpeg Convertor</h1>
-    <p className="text-secondary-foreground mt-2">Easily convertor, powered by ffmpeg.</p>
-    </>
+export default function Header() {
+    return <div className="flex justify-between items-center">
+        <div className="left">
+            <h1 className={cn("text-2xl", "font-semibold", "text-foreground","flex items-center")}>FFmpeg Convertor
+                <a href="https://github.com/joisun/ffmpeg-convertor" target="_blank" rel="noopener noreferrer" className="ml-4 flex items-center">
+                    <GitHubLogoIcon className="w-6 h-6 inline-block hover:text-primary/80" />
+                </a>
+            </h1>
+            <p className="text-secondary-foreground mt-2">Easily convertor, powered by ffmpeg.</p>
+        </div>
+        <div className="right">
+        </div>
+    </div>
 }
